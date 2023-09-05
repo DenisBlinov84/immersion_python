8eb68ad2fa5d1b376dac9b58cfc077c554a5dabf
Denis <blinovda1@mail.ru>
2023-09-05 08:11:59 +0300

    add lecture_1

---
+++ lecture_1.py
@@ -0,0 +1,30 @@
+# name = 'Alex'
+# age = None
+
+# a = 42
+# print(id(a))
+
+# a = 'Hello world'
+# print(id(a))
+
+# print(name, age, a, '456', 'text', sep=' (=^.^=) ', end='#')
+# print('Any text', end='\n')
+
+# res = input('Print your text: ')
+# print('Ты написал', res)
+
+# ADULT = 18
+# age = int(input('Сколько тебе лет ? '))
+# how_old = ADULT - age
+# print(how_old, 'Осталось до 18') 
+
+pwd = 'text'
+res = input('Input password: ')
+if res == pwd:
+    print('Доступ разрешён')
+    print('Но будьте осторожны')
+else:
+    print('Доступ запрещён')
+print('Работа завершена')
+
+
