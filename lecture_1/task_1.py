078f0658ed861fda1d6b8bde327c11b210487d32
Denis <blinovda1@mail.ru>
2023-09-06 16:36:52 +0300

    add lecture_1

---
+++ lecture_1/task_1.py
@@ -0,0 +1,43 @@
+# name = 'Alex'
+# age = None
+
+# a = 42
+# print(id(a))
+
+# a = 'Hello world'
+# print(id(a))
+
+# print(name, age, a, '456', 'text', sep=' (=^.^=) ', end='#')
+# print('Any text', end='\n')
+
+# res = input('Print your text: ')
+# print('Ты написал', res)
+
+# ADULT = 18
+# age = int(input('Сколько тебе лет ? '))
+# how_old = ADULT - age
+# print(how_old, 'Осталось до 18') 
+
+# pwd = 'text'
+# res = input('Input password: ')
+# if res == pwd:
+#     print('Доступ разрешён')
+#     my_math = int(input('2 + 2 = '))
+#     if 2 + 2 == my_math:
+#         print('Вы в нормальном мире')
+#     else:
+#         print('Но будьте осторожны')
+# else:
+#     print('Доступ запрещён')
+# print('Работа завершена')
+
+# color = input('Твой любимый цвет: ')
+# if color == 'красный':
+#     print('Любитель яркого')
+# elif color == 'зелёный':
+#     print('Ты не охотник?')
+# elif color == 'синий':
+#     print('Ха, классика!')
+# else:
+#     print('Тебя не понять')
+
