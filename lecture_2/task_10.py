9c313a37843e798993c518f0cb4d94c268aa6e85
Denis <blinovda1@mail.ru>
2023-09-12 18:12:01 +0300

    add task_10

--- lecture_2/task_10.py
+++ lecture_2/task_10.py
@@ -11,3 +11,15 @@
 # ● round(number[, ndigits]) — округляет число number до ndigits цифр
 # после запятой. Если второй аргумент не передать, округляет до ближайшего
 # целого.
+
+x = -42
+print(abs(x))
+
+a = 42
+b = 5
+print(divmod(a, b))
+
+print(pow(a, b))
+print(pow(a, b, 10))
+
+print(round(3.141_592_653_589_793, 3))
