70e62507311054230202245f50e6cb39cb624410
Denis <blinovda1@mail.ru>
2023-09-12 16:59:58 +0300

    add task_4

---
+++ lecture_2/task_4.py
@@ -0,0 +1,17 @@
+txt = 'Книга называется "Война и мир".'  # строка
+print(txt)
+
+
+class str(object):  # комментарий(без переменной)
+    """
+    str(object='') -> str
+    str(bytes_or_buffer[, encoding[, errors]] -> str
+    ...
+    """
+
+
+# ---------------------------------------------
+very_long_text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias animi assumenda at aut ' \
+                 'commodi, consequatur cumque ea harum, hic id illum ipsam itaque laboriosam magnam minus nam nulla ' \
+                 'numquam obcaecati officia officiis porro possimus praesentium quaerat temporibus ullam veniam? '
+print(very_long_text)
